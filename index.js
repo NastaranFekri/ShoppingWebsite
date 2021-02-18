d8502f0c7bc94ba318233243abb30e7393d6a002
Nastaran Fekri <fekri.nastaran@gmail.com>
2021-02-18 06:59:35 +0100

    fix style errors on index.js

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 class Product {
-    constructor(name, price, photos) {
-        this.name = name;
-        this.price = price;
-        this.photos = photos;
-    }
+  constructor(name, price, photos) {
+    this.name = name
+    this.price = price
+    this.photos = photos
+  }
 }
 
 class User {
-    constructor(name, familyName, address, email) {
-        this.name = name;
-        this.familyName = familyName;
-        this.address = address;
-        this.email = email;
-        this.invoices = [];
-    }
+  constructor(name, familyName, address, email) {
+    this.name = name
+    this.familyName = familyName
+    this.address = address
+    this.email = email
+    this.invoices = []
+  }
 }
 class Invoice {
-    constructor(products, user) {
-        this.products = products;
-        this.user = user;
-        this.timestamp = Date.now();
-    }
+  constructor(products, user) {
+    this.products = products
+    this.user = user
+    this.timestamp = Date.now()
+  }
 }
 
-const nastaran = new User('nastaran', 'f','berlin', 'f@gmail.com');
+const nastaran = new User('nastaran', 'f', 'berlin', 'f@gmail.com')
 
-const chair = new Product('leather chair', 150, ['chair.jpg']);
+const chair = new Product('leather chair', 150, ['chair.jpg'])
 
-const invoice = new Invoice([chair], nastaran);
-nastaran.invoices.push(invoice);
+const invoice = new Invoice([chair], nastaran)
+nastaran.invoices.push(invoice)
 
-console.log(nastaran);
+console.log(nastaran)
